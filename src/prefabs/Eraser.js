2e1f1643ed7f00876bc7033f6f43bbfd02fec2ab
Elroy <lsaltzherr@gmail.com>
2022-05-01 00:03:09 -0700

    made small adjustments to eraser, fixed when eraser goes off screen to not be stuck there

--- src/prefabs/Eraser.js
+++ src/prefabs/Eraser.js
@@ -1,13 +1,13 @@
-class Eraser extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene, x, y, texture){
+class Eraser extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.setImmovable(true);
         this.accel = 0.01;
-        this.speed = 2;
+        this.speed = 1;
         this.maxspeed = 10;
-        this.setVelocity(this.speed,0);
+        this.setVelocity(this.speed, 0);
         this.scale = 1;
         //this.flipY = true;
 
@@ -19,20 +19,28 @@ class Eraser extends Phaser.Physics.Arcade.Sprite{
         });
         this.anims.play('erase', true);
     }
-    update(){
-        //this.accel += 0.001;
+    update() {
+        
+       
         this.speed += this.accel;
-        this.maxspeed += 0.05;
-        if(this.speed > this.maxspeed){
+        if (this.speed > this.maxspeed) {
             this.speed = this.maxspeed;
         }
-        if(this.x + this.width <= 0){
-            this.x = 0 - this.width;
+        if (this.x + this.width  < 10) {
+            this.x = 10 - this.width;
             this.speed = 0;
+            console.log("End");
+        }
+        if(this.scene.player.score > 10){
+            this.maxspeed += 5;
+        }
+        if(this.scene.player.score > 25){
+            this.maxspeed += 10
         }
         this.setVelocity(this.speed, 0);
+        
     }
-    
+
     gameOver() {
         // move back as to not constantly trigger gameOver() in Play
         this.x -= 1
--- src/prefabs/Letterspawner.js
+++ src/prefabs/Letterspawner.js
@@ -1,6 +1,6 @@
-randomlocations = [240,320,400];
-class Letterspawner extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene, x, y, texture){
+randomlocations = [240, 320, 400];
+class Letterspawner extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         scene.add.existing(this);
         scene.physics.add.existing(this);
@@ -16,7 +16,7 @@ class Letterspawner extends Phaser.Physics.Arcade.Sprite{
         this.hello = 0;
     }
 
-    update(){
+    update() {
         // Go Up and Down
         //if(this.y <= this.highest || this.y >= this.lowest){
         //    this.movespeed *=-1;
@@ -24,22 +24,22 @@ class Letterspawner extends Phaser.Physics.Arcade.Sprite{
         //this.y += this.movespeed;
         // Spawn a new Letter based off of a timer
         this.timer -= 1;
-        if(this.timer <= 0){
+        if (this.timer <= 0) {
             this.timer = this.startingtimer;
-            var letter = new Letter(this.scene, this.x, this.y).setOrigin(0,0);
+            var letter = new Letter(this.scene, this.x, this.y).setOrigin(0, 0);
             this.lettersGroup.add(letter);
-            letter.setVelocity(letter.speed,0);
+            letter.setVelocity(letter.speed, 0);
             letter.init();
             this.randomMove();
         }
     }
-    randomMove(){
+    randomMove() {
         var y = this.getRandomInt(randomlocations.length)
         this.setY(randomlocations[y]);
     }
     getRandomInt(max) {
         return Math.floor(Math.random() * max);
-      }
+    }
     gameOver() {
         // clear letter group to get rid of any letters left on screen
         this.lettersGroup.clear(1, 1);
